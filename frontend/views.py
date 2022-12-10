bbcb9c3b8755d236ac734f79cfe983e40fb56dbb
fredtruix <fredricktrust@gmail.com>
2022-12-10 11:05:41 +0100

    status: login completed

--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import UserRegisterFrom
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 
@@ -36,6 +39,7 @@ class registerView(View):
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
+            return redirect('login')
         return render(request, self.template_name, {"user":user} )
 
 
@@ -52,5 +56,12 @@ class LoginView(View):
     def post(self, request, *args, **kwargs) -> render:
         email = request.POST.get("email")
         password = request.POST.get("password")
-        print(email)
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, "User does not exist")
+        user = authenticate(request, username=user.username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
         return render(request, self.template_name)
