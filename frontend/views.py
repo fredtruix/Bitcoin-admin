38095e47fd28981877cc77b083cf1edbed6ac57b
fredtruix <fredricktrust@gmail.com>
2022-12-12 12:57:53 +0100

    add: generation of btc address for users

--- frontend/views.py
+++ frontend/views.py
@@ -5,16 +5,16 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from bitcoin import *
+from .models import B_users
 
 
 # Create your views here.
 
 
-
 class Dashboard(LoginRequiredMixin, View):
     template_name = "frontend/index.html"
     login_url = 'login'
-
 
     def get(self, request, *args, **kwargs) -> render:
         return render(request, self.template_name)
@@ -27,27 +27,34 @@ class createUserView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
 
-
     def post(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        private_key = random_key()
+        public_key = privtopub(private_key)
+        address = pubkey_to_address(public_key)
+        B_users.objects.create(
+            fullName=request.POST.get('full_name'),
+            email=request.POST.get("email"),
+            phone_number=request.POST.get("phone"),
+            private_key=private_key,
+            public_key=public_key,
+            Btc_address=address
+        )
+        return redirect('createuser')
+       
 
 
-
-class registerView( View):
+class registerView(View):
     template_name = "frontend/register_login.html"
-
 
     def get(self, request, *args, **kwargs) -> render:
         if request.user.is_authenticated:
             return redirect('dashboard')
         form = UserRegisterFrom()
         context = {
-            "form":form,
-            "register":"register"
+            "form": form,
+            "register": "register"
         }
-        return render(request, self.template_name, context )
-
-
+        return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs) -> render or redirect:
         form = UserRegisterFrom(request.POST)
@@ -56,20 +63,17 @@ class registerView( View):
             user.username = user.username.lower()
             user.save()
             return redirect('login')
-        return render(request, self.template_name, {"user":user} )
-
+        return render(request, self.template_name, {"user": user})
 
 
 class LoginView(View):
-    template_name = "frontend/register_login.html"  
+    template_name = "frontend/register_login.html"
 
     def get(self, request, *args, **kwargs) -> render:
         if request.user.is_authenticated:
             return redirect('dashboard')
-      
+
         return render(request, self.template_name)
-
-
 
     def post(self, request, *args, **kwargs) -> render or redirect:
         email = request.POST.get("email")
@@ -83,6 +87,7 @@ class LoginView(View):
             login(request, user)
             return redirect('dashboard')
         return render(request, self.template_name)
+
 
 class LogoutView(View):
 
