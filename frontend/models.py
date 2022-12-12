795c9c04104a7d1d169bbce31b2e7a07799a7742
fredtruix <fredricktrust@gmail.com>
2022-12-12 03:08:31 +0100

    add: bitcoin db model

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class B_users(models.Model):
+    fullName = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=38)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f'self.fullName'
